2bff837ad7ed7f304dd47c6b91a8bbe3725a04e9
wallaC-137 <wallace_031@hotmail.com>
2023-08-22 15:03:23 -0300

    req03: ok - implementa a classe "PessoaEleitora"

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,46 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+/**
+ * The type Pessoa eleitora.
+ */
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  /**
+   * Instantiates a new Pessoa eleitora.
+   *
+   * @param nome the nome
+   * @param cpf  the cpf
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  /**
+   * Gets cpf.
+   *
+   * @return the cpf
+   */
+  public String getCpf() {
+    return cpf;
+  }
+
+  /**
+   * Sets cpf.
+   *
+   * @param cpf the cpf
+   */
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getNome() {
+    return super.getNome();
+  }
+
+  public void setNome(String nome) {
+    super.setNome(nome);
+  }
 }
