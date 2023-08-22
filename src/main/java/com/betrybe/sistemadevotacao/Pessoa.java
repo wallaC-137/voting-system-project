52f978eb9891a85b0e7a877c610462cae9390bde
wallaC-137 <wallace_031@hotmail.com>
2023-08-22 14:57:10 -0300

    comentário javadoc

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,13 +1,29 @@
 package com.betrybe.sistemadevotacao;
 
+/**
+ * The type Pessoa.
+ */
 public abstract class Pessoa {
 
+  /**
+   * The Nome.
+   */
   protected String nome;
 
+  /**
+   * Gets nome.
+   *
+   * @return the nome
+   */
   public String getNome() {
     return nome;
   }
 
+  /**
+   * Sets nome.
+   *
+   * @param nome the nome
+   */
   public void setNome(String nome) {
     this.nome = nome;
   }
--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,33 +1,65 @@
 package com.betrybe.sistemadevotacao;
 
 
+/**
+ * The type Pessoa candidata.
+ */
 public class PessoaCandidata extends Pessoa {
 
   private int numero;
   private int votos;
 
+  /**
+   * Instantiates a new Pessoa candidata.
+   *
+   * @param nome   the nome
+   * @param numero the numero
+   */
   public PessoaCandidata(String nome, int numero) {
     this.nome = nome;
     this.numero = numero;
     this.votos = 0;
   }
 
+  /**
+   * Gets numero.
+   *
+   * @return the numero
+   */
   public int getNumero() {
     return numero;
   }
 
+  /**
+   * Sets numero.
+   *
+   * @param numero the numero
+   */
   public void setNumero(int numero) {
     this.numero = numero;
   }
 
+  /**
+   * Gets votos.
+   *
+   * @return the votos
+   */
   public int getVotos() {
     return votos;
   }
 
+  /**
+   * Sets votos.
+   *
+   * @param votos the votos
+   */
   public void setVotos(int votos) {
     this.votos = votos;
   }
 
+  /**
+   * Receber voto.
+   */
   public void receberVoto() {
     this.votos++;
   }
